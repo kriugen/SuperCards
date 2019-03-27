ac13421678fcba5919fb44fa7f533294db6f321c
Ievgen Kriuchkov <eugene.kr@gmail.com>
2019-03-27 16:38:57 +0400

    fix samples parsing

--- client/Version.tsx
+++ client/Version.tsx
@@ -1,2 +1,2 @@
 import * as React from 'react'
-export default () => <div style={{ textAlign: 'right', marginRight: 10 }}>1.0.1</div>
+export default () => <div style={{ textAlign: 'right', marginRight: 10 }}>1.0.2</div>
